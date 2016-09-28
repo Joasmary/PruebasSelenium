6f0e5fcfd93362ea79492c0bc32827d519a2bfac
Joasmary <joasmary.perales@alaya.cl>
2016-09-28 18:54:14 -0300

    dfd
    fdsfa

--- src/main/java/example/Pruebas.java
+++ src/main/java/example/Pruebas.java
@@ -1,5 +1,12 @@
 package example;
+import com.google.gson.Gson;
 
-public class Pruebas {
-
+public class Pruebas
+{
+    public static void main( String[] args )
+    {
+            Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
 }
+
--- src/test/java/example/RecorridoCencosudFirefox.java
+++ src/test/java/example/RecorridoCencosudFirefox.java
@@ -29,12 +29,12 @@ public class RecorridoCencosudFirefox {
 	@BeforeClass(alwaysRun = true)
 	public void setUp() throws Exception {
 
-		DesiredCapabilities capabilities = DesiredCapabilities.htmlUnit();
+		DesiredCapabilities capabilities = DesiredCapabilities.firefox();
 		capabilities.setCapability("jenkins.label","linux");
 		//capabilities.setCapability("jenkins.nodeName","master");
 		capabilities.setJavascriptEnabled(true);
 		capabilities.setCapability(CapabilityType.ForSeleniumServer.ENSURING_CLEAN_SESSION, true);
-		driver = new RemoteWebDriver(new URL("http://192.168.0.160:5555/wd/hub"), capabilities);
+		driver = new RemoteWebDriver(new URL("http/192.168.0.109:4445/wd/hub"), capabilities);
 		baseUrl = "http://192.168.0.90:1337/";
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
